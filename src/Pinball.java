a3f5ff68fbafc46a9e22c5c79a35eea813f05ee9
Elan Levin <elanlevin1@gmail.com>
2022-11-16 13:33:08 -0600

    pinball class

---
+++ src/Pinball.java
@@ -0,0 +1,15 @@
+import edu.macalester.graphics.CanvasWindow;
+
+public class Pinball {
+    private static final int CANVAS_WIDTH = 600;
+    private static final int CANVAS_HEIGHT = 800;
+    CanvasWindow canvas;
+    
+    public Pinball() {
+        canvas = new CanvasWindow("Pinball", CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+
+    public static void main(String[] args) {
+        new Pinball();
+    }
+}
