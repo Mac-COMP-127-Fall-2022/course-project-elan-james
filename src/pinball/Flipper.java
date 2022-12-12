beb0874acceac2074f093fbfec2708fe682a014e
James-J-McCarthy <112642209+James-J-McCarthy@users.noreply.github.com>
2022-12-12 14:09:37 -0600

    Revert "Flipper images created and anchors started"
    This reverts commit 7f945bf2f30a85e3d1326d620c11083b526e8c17.

--- src/pinball/Flipper.java
+++ src/pinball/Flipper.java
@@ -2,39 +2,23 @@ package pinball;
 
 import java.awt.Color;
 
-import edu.macalester.graphics.CanvasWindow;
-import edu.macalester.graphics.GraphicsObject;
-import edu.macalester.graphics.Image;
+import edu.macalester.graphics.GraphicsGroup;
 import edu.macalester.graphics.Rectangle;
 
 public class Flipper {
     private Rectangle flipper;
     private boolean isPressed;
-    private double width, height;
    
-    public Flipper(double x, double y, boolean isOnTheLeft, double angle, CanvasWindow canvas) {
-        width = 90;
-        height = 10;
+    public Flipper(double x, double y, boolean isOnTheLeft, double angle, GraphicsGroup rectangleLayer) {
+        flipper = new Rectangle(x, y, 70, 10);
         isPressed = false;
-        
-        GraphicsObject image = new Image(x, y, "flipper.png");
-        image.setAnchor(x + 5, y + height/2);
-        image.setScale(.225);
-        if (isOnTheLeft) {
-            image.rotateBy(180);
-        }
-        canvas.add(image);
-        
-        flipper = new Rectangle(x, y + height/2, 90, 10);
-        flipper.setAnchor(x + 5, y + height/2);
         flipper.setFillColor(Color.BLACK);
-
         if (!isOnTheLeft) {
             flipper.rotateBy(180 + angle);  
         } else { 
             flipper.rotateBy(angle);  
         }
-        canvas.add(flipper);
+        rectangleLayer.add(flipper);
     }
 
     public void movePaddleUp(double angle) {
